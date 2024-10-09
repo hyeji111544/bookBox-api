e43160d2d0a3715630f0b8d18579e9580958977a
kimsoapsoap <zerfa@naver.com>
2024-10-10 01:48:40 +0900

    chore: sql 책 더미데이터 추가

--- src/main/java/green/mtcoding/bookbox/book/Book.java
+++ src/main/java/green/mtcoding/bookbox/book/Book.java
@@ -1,9 +1,6 @@
 package green.mtcoding.bookbox.book;
 import green.mtcoding.bookbox.comment.Comment;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,16 +19,21 @@ public class Book {
     private String title;
     private String author;
     private String publisher;
+
+    @Lob //description이 길어지면 VarChar(255)로 감당이 안 되는 녀석도 있어서 Text로 변경. 책 소개 다 들고 오면 길이 길어지므로 Lob붙여서 Text로 컬럼 생성
     private String description;
     private String categoryId;
     private String categoryName;
     private Integer itemId;
     private String cover;
     private String pubDate;
-    private int stock;
-
-    private boolean rentStatus;
+    private boolean lendStatus;
     private boolean adult;
+
+
+    private String link;
+    private boolean reservationStatus;
+
 
     @OneToMany(mappedBy = "book")
     private List<Comment> comments = new ArrayList<>();
