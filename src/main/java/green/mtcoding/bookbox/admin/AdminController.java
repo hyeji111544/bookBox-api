8b1570b80b4f648d6f9641060e687ce0942e10f5
Shin Min Jae <163399297+shnmj@users.noreply.github.com>
2024-10-11 18:39:35 +0900

    feat : 관리자-도서 CRUD 테스트 완료 (#13)
    * 로그아웃 테스트 완료 및 리팩토링 완료
    
    * rebase위한 중간 저장
    
    * 관리자-도서CRUD 테스트 완료

--- src/main/java/green/mtcoding/bookbox/admin/AdminController.java
+++ src/main/java/green/mtcoding/bookbox/admin/AdminController.java
@@ -1,11 +1,11 @@
 package green.mtcoding.bookbox.admin;
 
-
-import green.mtcoding.bookbox.core.global.CommonResponse;
-import green.mtcoding.bookbox.user.User;
+import green.mtcoding.bookbox.book.BookRequest;
+import green.mtcoding.bookbox.book.BookResponse;
+import green.mtcoding.bookbox.book.BookService;
+import green.mtcoding.bookbox.core.util.Resp;
 import green.mtcoding.bookbox.user.UserRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,20 +14,63 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
+
     private final AdminService adminService;
+    private final BookService bookService;
 
+    // =========================== AUTH ====================================
+    // 로그인
     @PostMapping("/api/admins/login")
-    public ResponseEntity<CommonResponse<AdminResponse.LoginResponse>> login(@RequestBody AdminRequest.LoginDTO request) {
-        AdminResponse.LoginResponse response = adminService.login(request);
-        return new ResponseEntity<>(CommonResponse.success(response), HttpStatus.OK);
+    public ResponseEntity<?> login(@RequestBody AdminRequest.LoginDTO loginDTO) {
+        AdminResponse.LoginDTO result = adminService.로그인(loginDTO);
+        String accessToken = result.getToken();
+
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer " + accessToken)
+                .body(Resp.ok(result));
     }
 
+    // 로그아웃
+    @PostMapping("/api/admins/logout")
+    public ResponseEntity<?> logout() {
+        // JWT 토큰 삭제 방식
+        return ResponseEntity.ok(Resp.ok("로그아웃이 완료되었습니다."));
+    }
+
+    // 전체 유저 목록 조회
     @GetMapping("/api/admins/user-list")
-    public ResponseEntity<CommonResponse<List<UserRequest.UserDTO>>> getUserList() {
+    public ResponseEntity<?> getUserList() {
         List<UserRequest.UserDTO> users = adminService.getUserList();
-        return new ResponseEntity<>(CommonResponse.success(users), HttpStatus.OK);
+        return ResponseEntity.ok(Resp.ok(users));
     }
 
-    // TODO: 추후 다른 crud 관련 API 추가 예정 (admin auth 마무리 하고)
 
+    // =========================== BOOK ====================================
+    // 도서 등록
+    @PostMapping("/api/admins/book-save")
+    public ResponseEntity<?> saveBook(@RequestBody BookRequest.SaveDTO saveDTO) {
+        BookResponse.BookDetailDTO savedBook = bookService.도서등록(saveDTO);
+        return ResponseEntity.ok(Resp.ok(savedBook));
+    }
+
+    // 도서 수정
+    @PutMapping("/api/admins/{isbn13}")
+    public ResponseEntity<?> updateBook(@PathVariable String isbn13, @RequestBody BookRequest.UpdateDTO updateDTO) {
+        BookResponse.BookDetailDTO updateBook = bookService.도서업데이트(isbn13, updateDTO);
+        return ResponseEntity.ok(Resp.ok(updateBook));
+    }
+
+    // 도서 삭제
+    @DeleteMapping("/api/admins/{isbn13}/delete")
+    public ResponseEntity<?> deleteBook(@PathVariable String isbn13) {
+        bookService.deleteBook(isbn13);
+        return ResponseEntity.ok(Resp.ok("도서가 성공적으로 삭제되었습니다."));
+    }
+
+    // 도서 상세보기 (등록 도서 조회)
+    @GetMapping("/api/admins/{isbn13}")
+    public ResponseEntity<?> getBookDetails(@PathVariable String isbn13) {
+        BookResponse.BookDetailDTO bookDetail = bookService.도서상세보기(isbn13);
+        return ResponseEntity.ok(Resp.ok(bookDetail));
+    }
 }
--- src/main/java/green/mtcoding/bookbox/book/Book.java
+++ src/main/java/green/mtcoding/bookbox/book/Book.java
@@ -44,4 +44,28 @@ public class Book {
     @OneToMany(mappedBy = "book")
     private List<Comment> comments = new ArrayList<>();
 
+    // TODO: BookRequest.UpdateDTO를 받아서 엔티티를 업데이트하는 메소드
+    public Book(String isbn13, String title, String author, String publisher, String description, String cover, String pubDate, Category category) {
+        this.isbn13 = isbn13;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.description = description;
+        this.cover = cover;
+        this.pubDate = pubDate;
+        this.category = category;
+        this.lendStatus = false; // 대여 가능
+    }
+
+    public void update(BookRequest.UpdateDTO dto, Category category) {
+        this.title = dto.getTitle();
+        this.author = dto.getAuthor();
+        this.publisher = dto.getPublisher();
+        this.description = dto.getDescription();
+        this.cover = dto.getCover();
+        this.pubDate = dto.getPubDate();
+        this.category = category; // 카테고리 업데이트
+    }
+
+
 }
