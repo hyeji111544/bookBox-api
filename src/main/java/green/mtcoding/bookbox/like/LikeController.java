93a8078ee4fbbac5872b3b87f86b102b41701811
Hojeong <queen00220@naver.com>
2024-10-15 19:26:23 +0900

    feat: Transactional 어노테이션 서비스에 추가

--- src/main/java/green/mtcoding/bookbox/like/LikeController.java
+++ src/main/java/green/mtcoding/bookbox/like/LikeController.java
@@ -1,10 +1,20 @@
 package green.mtcoding.bookbox.like;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class LikeController {
+
     private final LikeService likeService;
+
+    // 즐겨찾기 추가
+    @PostMapping("/api/likes")
+    public void likeDo(@RequestHeader("Authorization") String token){
+
+    }
+
 }
--- src/main/java/green/mtcoding/bookbox/like/LikeService.java
+++ src/main/java/green/mtcoding/bookbox/like/LikeService.java
@@ -2,10 +2,14 @@ package green.mtcoding.bookbox.like;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class LikeService {
+
     private final LikeRepository likeRepository;
+
 
 }
